c9d3b312de42dd58649e3294872338ed82f31a93
ifgovtjoftibcdjpvd8nfiokbfobffob0vrb8bd <sonkarp463@gmail.com>
2025-05-23 04:51:33 +0530

    Update __main__.py
    Signed-off-by: ifgovtjoftibcdjpvd8nfiokbfobffob0vrb8bd <sonkarp463@gmail.com>

--- AnonXMusic/__main__.py
+++ AnonXMusic/__main__.py
@@ -5,11 +5,11 @@ from pyrogram import idle
 from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
-from AnonXMusicMUSIC import LOGGER, app, userbot
-from AnonXMusicMUSIC.core.call import AnonXMusic
-from AnonXMusicMUSIC.misc import sudo
-from AnonXMusicMUSIC.plugins import ALL_MODULES
-from AnonXMusicMUSIC.utils.database import get_banned_users, get_gbanned
+from AnonXMusic import LOGGER, app, userbot
+from AnonXMusic.core.call import Anony
+from AnonXMusic.misc import sudo
+from AnonXMusic.plugins import ALL_MODULES
+from AnonXMusic.utils.database import get_banned_users, get_gbanned
 from config import BANNED_USERS
 
 
@@ -35,27 +35,27 @@ async def init():
         pass
     await app.start()
     for all_module in ALL_MODULES:
-        importlib.import_module("AnonXMusicMUSIC.plugins" + all_module)
-    LOGGER("AnonXMusicMUSIC.plugins").info("𝐀𝐥𝐥 𝐅𝐞𝐚𝐭𝐮𝐫𝐞𝐬 𝐋𝐨𝐚𝐝𝐞𝐝 𝐁𝐚𝐛𝐲🥳...")
+        importlib.import_module("AnonXMusic.plugins" + all_module)
+    LOGGER("AnonXMusic.plugins").info("𝐀𝐥𝐥 𝐅𝐞𝐚𝐭𝐮𝐫𝐞𝐬 𝐋𝐨𝐚𝐝𝐞𝐝 𝐁𝐚𝐛𝐲🥳...")
     await userbot.start()
-    await AnonXMusic.start()
+    await Anony.start()
     try:
-        await AnonXMusic.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
+        await Anony.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
     except NoActiveGroupCall:
-        LOGGER("AnonXMusicMUSIC").error(
+        LOGGER("AnonXMusic").error(
             "𝗣𝗹𝗭 𝗦𝗧𝗔𝗥𝗧 𝗬𝗢𝗨𝗥 𝗟𝗢𝗚 𝗚𝗥𝗢𝗨𝗣 𝗩𝗢𝗜𝗖𝗘𝗖𝗛𝗔𝗧\𝗖𝗛𝗔𝗡𝗡𝗘𝗟\n\n𝗧𝗠𝗠 𝗕𝗢𝗧 𝗦𝗧𝗢𝗣........"
         )
         exit()
     except:
         pass
-    await AnonXMusic.decorators()
-    LOGGER("AnonXMusicMUSIC").info(
+    await Anony.decorators()
+    LOGGER("AnonXMusic").info(
         "╔═════ஜ۩۞۩ஜ════╗\n  ☠︎︎𝗠𝗔𝗗𝗘 𝗕𝗬 𝗠𝗥 𝗡𝗢𝗕𝗜☠︎︎\n╚═════ஜ۩۞۩ஜ════╝"
     )
     await idle()
     await app.stop()
     await userbot.stop()
-    LOGGER("AnonXMusicMUSIC").info("𝗦𝗧𝗢𝗣 𝗧𝗠𝗠 𝗠𝗨𝗦𝗜𝗖🎻 𝗕𝗢𝗧..")
+    LOGGER("AnonXMusic").info("𝗦𝗧𝗢𝗣 𝗧𝗠𝗠 𝗠𝗨𝗦𝗜𝗖🎻 𝗕𝗢𝗧..")
 
 
 if __name__ == "__main__":
