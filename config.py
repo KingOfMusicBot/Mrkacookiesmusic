1646f2ae7bf82cd56e06c776dcfeb954cf9beaaf
ifgovtjoftibcdjpvd8nfiokbfobffob0vrb8bd <sonkarp463@gmail.com>
2025-05-23 13:09:07 +0530

    Update config.py
    Signed-off-by: ifgovtjoftibcdjpvd8nfiokbfobffob0vrb8bd <sonkarp463@gmail.com>

--- config.py
+++ config.py
@@ -17,8 +17,11 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
 # Vars For API End Pont.
-YTPROXY_URL = getenv("YTPROXY_URL", 'https://api.thequickearn.xyz') ## E.G https://yt.okflix.
-YT_API_KEY = "NxGBNexGenBots0e3463"
+
+
+API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') 
+API_KEY = getenv("API_KEY", 'NxGBNexGenBots0e3463')
+
 
 ## Other vaes
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 300))
